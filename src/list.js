ed01dfaba544facaf8789a80374fe20f6494b5f5
Michael <39811434+msawlani@users.noreply.github.com>
2020-02-12 15:39:28 -0600

    list
    added a list to display an item to tell which js file you are on

---
+++ src/list.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+const List = () => {
+  return (
+    <div>
+      <ul>
+        <li>First Item</li>
+      </ul>
+    </div>
+  );
+};
+
+export default List;
