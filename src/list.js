7fa6488b07708f41d8c31436785cc0910b7d84f5
Michael <39811434+msawlani@users.noreply.github.com>
2020-02-13 15:30:46 -0600

    input and add to list
    made a input box so you can add things to the list

--- src/list.js
+++ src/list.js
@@ -2,11 +2,28 @@ import React from "react";
 import "./App.css";
 
 const List = () => {
+  const [getItem, setItem] = React.useState("");
+  const [getItems, setItems] = React.useState([]);
+
+  const handleItem = event => {
+    setItem({ item: event.target.value });
+  };
+
+  const handleItems = () => {
+    setItems(items => [...items, Object.values(getItem)]);
+  };
+
+  let itemList = Object.values(getItems.map(item => <li>{item}</li>));
   return (
     <div>
-      <ul>
-        <li>First Item</li>
-      </ul>
+      <input
+        placeholder="item"
+        name="item"
+        value={Object.values(getItem)}
+        onChange={handleItem}
+      />
+      <button onClick={handleItems}>Add</button>
+      <ul>{itemList}</ul>
     </div>
   );
 };
